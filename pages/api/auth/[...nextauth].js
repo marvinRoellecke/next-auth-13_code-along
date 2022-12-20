5e3bb0fc5da40f42338b11ad533c58f4a19635ab
Marvin Roellecke <marvin@roellecke.com>
2022-12-20 12:54:48 +0100

    workaround login for preview

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,40 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const providers = [];
+
+if (process.env.VERCEL_ENV === "preview") {
+  providers.push(
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        username: {label: "Username", type: "text", placeholder: "username"},
+        password: {label: "Password", type: "password"},
+      },
+      async authorize(credentials) {
+        if (
+          credentials.username === "octopus" &&
+          credentials.password === "octopus"
+        ) {
+          return {
+            name: "Octopus Test User",
+            email: "test@example.com",
+          };
+        } else {
+          return null;
+        }
+      },
+    })
+  );
+} else {
+  providers.push(
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    })
+  );
+}
 
 export const authOptions = {
   // Configure one or more authentication providers
